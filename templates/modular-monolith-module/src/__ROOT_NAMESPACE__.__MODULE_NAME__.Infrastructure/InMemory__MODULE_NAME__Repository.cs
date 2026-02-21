7d358c8e492ff1d1220016d3cee23a95463ea855
Michele Ferracin <michele.ferracin@live.it>
2026-02-21 17:45:58 +0100

    feat: add dotnet template for modular monolith module

---
+++ templates/modular-monolith-module/src/__ROOT_NAMESPACE__.__MODULE_NAME__.Infrastructure/InMemory__MODULE_NAME__Repository.cs
@@ -0,0 +1,6 @@
+namespace __ROOT_NAMESPACE__.__MODULE_NAME__.Infrastructure;
+
+public sealed class InMemory__MODULE_NAME__Repository
+{
+    // TODO: replace with real persistence adapter (Mongo, Postgres, etc.)
+}
